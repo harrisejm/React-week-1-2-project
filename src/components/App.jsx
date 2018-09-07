c341767d88cf2717e39ecf4c8744a1bc7fb2171b
Eddie Harris <harrisejm@gmail.com>
2018-09-07 09:56:07 -0700

    initial file setup

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function App(){
+  return (
+    <div><Header/>
+    <p>Test</p>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header(){
+  return (
+    <h1>Eddie's Brew House</h1>
+  );
+}
+
+export default Header;
