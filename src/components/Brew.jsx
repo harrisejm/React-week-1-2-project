388ebc56b22b3b3acd7d993157289e8714b2ed1e
Eddie Harris <harrisejm@gmail.com>
2018-09-07 12:04:28 -0700

    add jsx loop to display drink info

--- src/components/Brew.jsx
+++ src/components/Brew.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+let brewStyles = {
+  width: "500px",
+  borderBottom: "solid"
+
+}
+
+
 function Brew(props){
+
   return (
-    <div>
-      <p>{props.name}</p>
-      <p>{props.type}</p>
-      <p>{props.price}</p>
-      <p>{props.inventory}</p>
+    <div style={brewStyles}>
+      <h3>{props.name}</h3>
+      <ul>
+        <li>{props.type}</li>
+        <li>{props.description}</li>
+        <li>{props.price}</li>
+        <li>{props.inventory}</li>
+      </ul>
     </div>
 
   );
@@ -16,6 +27,7 @@ function Brew(props){
 Brew.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
   inventory: PropTypes.string.isRequired
 };
--- src/components/BrewList.jsx
+++ src/components/BrewList.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import Brew from './Brew';
 
+let fullBrewList = [
+  {
+    name: 'Irish Coffee',
+    type: 'specialty drink',
+    description: 'Coffee With Wisky',
+    price: '$7.00',
+    inventory: '97'
+  },
+  {
+    name: 'NW Hefeweizen',
+    type: 'Hefeweizen',
+    description: 'Wheat Beer',
+    price: '$5.00',
+    inventory: '67'
+  },
+  {
+    name: 'NW Orange Hefeweizen',
+    type: 'Hefeweizen',
+    description: 'Wheat Beer with a hint of orange',
+    price: '$5.00',
+    inventory: '129'
+  }
+];
+
+
 function BrewList(){
   return (
-    <Brew
-      name = "Coffee"
-      type ="Energy"
-      price = "5.00"
-      inventory = "97"
-    />
-
+    <div>
+      { fullBrewList.map((brew, index) =>
+        <Brew name={brew.name}
+          type={brew.type}
+          description={brew.description}
+          price={brew.price}
+          inventory={brew.inventory}
+          key={index}/>
+      )}
+    </div>
   );
 }
 
